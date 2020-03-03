9281667e7e4d5f38028dd4877aae69d9f8ab6571
Marc Bradshaw <marc@marcbradshaw.net>
2020-03-03 09:49:55 +0000

    First commit

---
+++ lib/Mail/TLSRPT.pm
@@ -0,0 +1,14 @@
+package Mail::BIMI;
+# ABSTRACT: TLSRPT object
+# VERSION
+use 5.20.0;
+use Moo;
+use Carp;
+use Types::Standard qw{Str HashRef ArrayRef};
+use Type::Utils qw{class_type};
+use Mail::TLSRPT::Pragmas;
+
+
+
+1;
+
